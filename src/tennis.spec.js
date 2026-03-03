09009ea6955ba6ec8a7798c934104b8f08760d81
Sarah Cazorla <sarah.cazorla@ucb.edu.bo>
2026-03-02 23:03:56 -0400

    feat: Advantage for Player

--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -80,6 +80,29 @@ it("ambos jugadores anotan 3, Deuce", () => {
     expect(game.score()).toEqual("Deuce");
   });
 // Si el juego esta en Deuce jugador 1 anota => "Advantage for Player 1"
+it("desde Deuce jugador 1 anota deberia ser Advantage for Player 1", () => {
+    const game = new Tennis();
+    game.player1Scores();
+    game.player1Scores();
+    game.player1Scores();
+    game.player2Scores();
+    game.player2Scores();
+    game.player2Scores();
+    game.player1Scores();
+    expect(game.score()).toEqual("Advantage for Player 1");
+  });
+// Si el juego esta en Deuce jugador 2 anota => "Advantage for Player 2"
+  it("desde Deuce jugador 2 anota deberia ser Advantage for Player 2", () => {
+    const game = new Tennis();
+    game.player1Scores();
+    game.player1Scores();
+    game.player1Scores();
+    game.player2Scores();
+    game.player2Scores();
+    game.player2Scores();
+    game.player2Scores();
+    expect(game.score()).toEqual("Advantage for Player 2");
+  });
 // estado en Advantage del jugador 1 anota una vez mas => "Game For Player"
 });
 
